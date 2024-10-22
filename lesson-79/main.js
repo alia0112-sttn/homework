457160afe5484b5064fbd44f928bbe9894dcf07f
alia0112-sttn <sokolovaaleksandra294@gmail.com>
2024-10-22 23:12:58 +0300

    Add files via upload

---
+++ lesson-79/main.js
@@ -0,0 +1,58 @@
+// ====Условия====
+
+// ====if===>
+// const firstNumber = 5
+// const secondNumber = 5
+
+// if (firstNumber == secondNumber) {
+//     const result = firstNumber + secondNumber
+//     console.log(result)
+// }
+
+// ====else===>
+const firstNumber = 15
+const secondNumber = 7
+
+if (firstNumber == secondNumber) {
+    const result = firstNumber + secondNumber
+    console.log(result)
+} else {
+    const result = secondNumber - firstNumber
+    console.log(result)
+}
+
+// ====ДЗ===>
+
+let age = 21
+const yo = 18
+
+if (age >= yo) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вам нет 18 лет')
+}
+
+// ====тинарные операторы==>
+
+let myAge = 21
+const myYO = 18
+
+myAge >= (myYO) ? console.log('Вы совершеннолетний') : console.log('Вам нет 18 лет')
+
+// ====Мои попытки понять условия===>
+
+const fNumber = 100
+const sNumber = 50
+
+let result = null
+
+if( fNumber <= sNumber ){
+    console.log('Код красный')
+}else{
+    console.log('Код зеленый')
+}
+
+(fNumber) <= (sNumber) ? console.log('Код красный') : console.log('Код зеленый')
+
+// ==============
+
---
+++ lesson-80/main.js
@@ -0,0 +1,39 @@
+// ====ДЗ====
+
+// Четные числа===>
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+let a = 0
+
+while (a < 12) {
+    a = a + 2
+    console.log(a)
+}
+
+let g = 0
+
+do{
+    g = g + 2
+    console.log(g)
+}while(g <= 12)
+
+// От 10 до 1===>
+    for(let b = 10; b >= 1; b = b - 1){
+        console.log(b)
+    }
+
+    let c = 11
+
+    while (c > 1){
+        c = c - 1
+        console.log(c)
+    }
+
+    let y = 11
+    
+    do{
+        y = y - 1
+        console.log(y)
+    }while(y >= 1)
