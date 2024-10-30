bce80ec28fcf7a5b6a35a890361db6fa3ebc1545
alia0112-sttn <sokolovaaleksandra294@gmail.com>
2024-10-31 00:25:50 +0300

    Add files via upload

---
+++ lesson-80/main.js
@@ -0,0 +1,41 @@
+// ====ДЗ====
+
+// Четные числа===>
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+let a = 0
+
+while (a < 12) {
+    a = a + 2
+    console.log(a)
+}
+
+let g = 0
+
+do{
+    g = g + 2
+    console.log(g)
+}while(g <= 12)
+
+// От 10 до 1===>
+    for(let b = 10; b >= 1; b = b - 1){
+        console.log(b)
+    }
+
+// =========исправление
+    let c = 10
+
+    while (c > 0){
+        console.log(c);
+        c = c - 1;
+    }
+    
+    let y = 10
+    
+    do{
+        console.log(y);
+        y = y - 1;
+    }while(y > 0)
+    
