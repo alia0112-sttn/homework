1cbd1a8c49c5bb2127f2486ccb46aec067316494
alia0112-sttn <sokolovaaleksandra294@gmail.com>
2024-11-07 15:55:46 +0300

    Add files via upload

---
+++ lesson-84/main.js
@@ -0,0 +1,50 @@
+// Играюсь с кодом 1 =====>
+
+const fNum = 15
+const sNum = 5
+
+let result = null
+
+if (fNum === sNum) {
+    result = fNum + sNum
+}
+else {
+    result = fNum - sNum
+}
+
+console.log(result)
+
+// Играюсь с кодом 2 =====>
+
+const firstNum = 20
+const secondNum = 14
+
+let result_1 = null
+
+if (firstNum < secondNum) {
+    result_1 = firstNum * secondNum
+}
+else{
+    result_1 = firstNum - secondNum
+}
+
+console.log(result_1)
+
+// Играюсь с кодом 3 =====>
+
+const fNumber = 50 
+const sNumber = 48
+
+let result_2 = null 
+
+if (sNumber >= fNumber){
+    result_2 = fNumber - sNumber
+}
+else{
+    result_2 = sNumber * fNumber
+}
+
+console.log(result_2)
+
+
+
---
+++ lesson-85/main.js
@@ -0,0 +1,26 @@
+// Метод POP====>
+
+const num = [1, 2, 3, 4, 5, 'alex', 'tom'];
+
+const notNum = num.pop();
+
+console.log(notNum);
+console.log(num)
+
+// Метод shift ====>
+
+    const nam = ['alex', 'john', 'max','tomas', 'anna'];
+
+    const firstNam = nam.shift();
+
+    console.log(firstNam);
+    console.log(nam)
+
+    // Метод unshift====>
+
+        const age = [19, 20, 21, 22];
+
+        const total = age.unshift(17, 18);
+
+        console.log(total);
+        console.log(age)
